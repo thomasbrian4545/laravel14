491d7b64fc694dda054884b0bccf267888796b01
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-11-19 22:35:20 +0700

    Eloquent Lanjutan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Mahasiswa;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $faker = Faker::create('id_ID');
+        $faker->seed(123);
+        for ($i = 0; $i < 10; $i++) {
+            Mahasiswa::create(
+                [
+                    'nim' => $faker->unique()->numerify('10######'),
+                    'nama' => $faker->firstName() . " " . $faker->lastName(),
+                    'tanggal_lahir' => $faker->dateTimeInInterval(
+                        '1999-01-01',
+                        '+ 3 years'
+                    ),
+                    'ipk' => $faker->randomFloat(2, 2, 4),
+                    'created_at' => $faker->dateTimeBetween('-10 days', '-5 days'),
+                    'updated_at' => $faker->dateTimeBetween('-3 days'),
+                ]
+            );
+        }
     }
 }
